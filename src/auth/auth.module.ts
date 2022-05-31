65c551c59da1ef2d0b7c5bb646eb0ef2abc6a90e
A1doranM <vaselisk1997@gmail.com>
2022-05-31 16:16:20 +0300

    completed authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,14 @@ import {AuthService} from "./services/auth.service";
 import {JwtAssignTokenStrategy} from "./strategies/jwt-assign-token.strategy";
 import {JwtRefreshTokenStrategy} from "./strategies/jwt-refresh-token.strategy";
 import {JwtModule} from "@nestjs/jwt";
+import {AtGuards} from "./guards/at.guards";
+import {RtGuards} from "./guards/rt.guards";
 
 @Module({
     imports: [JwtModule.register({})],
     controllers: [AuthController],
-    providers: [AuthService, JwtAssignTokenStrategy, JwtRefreshTokenStrategy]
+    providers: [AuthService, JwtAssignTokenStrategy, JwtRefreshTokenStrategy],
+    exports: [AtGuards, RtGuards]
 })
 export class AuthModule {
 }
